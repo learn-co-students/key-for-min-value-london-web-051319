8c56fa0fe4ec7ad673473f1bae7f3ec9a79567c0
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 11:15:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   low_key = nil
-  low_value = nil
+  low_value = Float::INFINITY
   
   name_hash.each do |key, value|
-    if  value < low_value
+    if value < low_value
       low_value = value
       low_key = key
     end
