2cb4740a2de4f4894881bf429bf64508bec9a557
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-10 18:04:46 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
-  name_hash.each do |k, v|
+  name_hash.collect do |k, v|
     if lowest_value == nil || v < lowest_value
       lowest_value = v 
       lowest_key = k 
     end 
-  end 
+   end
   lowest_key
 end 
 
